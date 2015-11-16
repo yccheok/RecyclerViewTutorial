4ab8527312fd48e925fee115ef09aff622b6b3c2
Yan Cheng Cheok <yccheok@yahoo.com>
2015-11-17 00:21:43 +0800

    Added empty recycler view.

--- app/src/main/java/org/yccheok/recyclerviewtutorial/MainFragment.java
+++ app/src/main/java/org/yccheok/recyclerviewtutorial/MainFragment.java
@@ -2,6 +2,8 @@ package org.yccheok.recyclerviewtutorial;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +12,24 @@ import android.view.ViewGroup;
  * Created by yccheok on 16/11/2015.
  */
 public class MainFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
         return view;
     }
 }
