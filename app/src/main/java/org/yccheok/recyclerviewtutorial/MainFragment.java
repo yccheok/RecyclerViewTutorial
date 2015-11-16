3d4677ac5054fb5f97739960eecda8946fa658f9
Yan Cheng Cheok <yccheok@yahoo.com>
2015-11-16 23:46:44 +0800

    Added toolbar and fragment.

---
+++ app/src/main/java/org/yccheok/recyclerviewtutorial/MainFragment.java
@@ -0,0 +1,19 @@
+package org.yccheok.recyclerviewtutorial;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yccheok on 16/11/2015.
+ */
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
